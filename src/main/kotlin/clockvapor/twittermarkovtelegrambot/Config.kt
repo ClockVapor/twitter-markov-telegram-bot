32963844bf4db7dc006d72a03bde13e94e706495
Nick Lowery <ClockVapor@users.noreply.github.com>
2018-06-20 14:37:55 -0600

    Working Telegram bot side

---
+++ src/main/kotlin/clockvapor/twittermarkovtelegrambot/Config.kt
@@ -0,0 +1,7 @@
+package clockvapor.twittermarkovtelegrambot
+
+class Config {
+    lateinit var telegramBotToken: String
+    lateinit var twitterKey: String
+    lateinit var twitterSecret: String
+}
