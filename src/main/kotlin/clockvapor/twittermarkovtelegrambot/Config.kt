7a824061e04268aa05c7b914c9f81d148aeb08db
Nick Lowery <ClockVapor@users.noreply.github.com>
2018-07-15 19:05:11 -0600

    Update Kotlin and kotlin-telegram-bot versions; other cleanup

--- src/main/kotlin/clockvapor/twittermarkovtelegrambot/Config.kt
+++ src/main/kotlin/clockvapor/twittermarkovtelegrambot/Config.kt
@@ -1,5 +1,9 @@
 package clockvapor.twittermarkovtelegrambot
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import java.io.File
+
 class Config {
     lateinit var telegramBotToken: String
     lateinit var twitterUsername: String
@@ -7,4 +11,11 @@ class Config {
     lateinit var twitterConsumerSecret: String
     lateinit var twitterAccessToken: String
     lateinit var twitterAccessSecret: String
+
+    companion object {
+        fun read(path: String): Config {
+            val mapper = ObjectMapper(YAMLFactory())
+            return mapper.readValue<Config>(File(path), Config::class.java)
+        }
+    }
 }
--- src/main/kotlin/clockvapor/twittermarkovtelegrambot/Main.kt
+++ src/main/kotlin/clockvapor/twittermarkovtelegrambot/Main.kt
@@ -1,28 +1,20 @@
 package clockvapor.twittermarkovtelegrambot
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.mainBody
-import java.io.File
 import kotlin.concurrent.thread
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) = mainBody {
         val a = ArgParser(args).parseInto(::Args)
-        val config = loadConfig(a.configPath)
+        val config = Config.read(a.configPath)
         thread { TelegramBot.run(config.telegramBotToken, a.dataPath) }
         thread {
             TwitterScraper.run(config.twitterUsername, config.twitterConsumerKey, config.twitterConsumerSecret,
                 config.twitterAccessToken, config.twitterAccessSecret, a.dataPath)
         }
         Unit
-    }
-
-    private fun loadConfig(path: String): Config {
-        val mapper = ObjectMapper(YAMLFactory())
-        return mapper.readValue<Config>(File(path), Config::class.java)
     }
 
     class Args(parser: ArgParser) {
